8b58c8761f9d714b1af81264f069061bc3740450
Toha810 <tohahpro@gmail.com>
2023-11-24 15:21:59 +0600

    set up in mongodb, install cors, mongodb,dotenv, set up cors and also create products post api

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express')
+require('dotenv').config()
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 5000
+
+
+
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ruzxtxl.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+
+        const productsCollection = client.db('trendNestDB').collection('products');
+
+
+        app.post('/menu', async (req, res) => {
+            const item = req.body;
+            const result = await productsCollection.insertOne(item);
+            res.send(result)
+        })
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
