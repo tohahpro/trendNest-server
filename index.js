5b981f57175d77f8937055c6f35e608f4fa4a71d
Toha810 <tohahpro@gmail.com>
2023-11-29 16:25:30 +0600

    extran line delete

--- index.js
+++ index.js
@@ -453,8 +453,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
 
     }
